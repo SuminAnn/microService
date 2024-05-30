eab45924f5809c035d47e28bf25e61504e8b0cb6
SuminAnn <annsumin2066@naver.com>
2024-05-30 15:34:41 +0900

    spring cloud gateway filter

--- user-service/first-service/src/main/java/com/example/first_service/FirstServiceController.java
+++ user-service/first-service/src/main/java/com/example/first_service/FirstServiceController.java
@@ -1,15 +1,25 @@
 package com.example.first_service;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
+@Slf4j
 @RequestMapping("/first-service")
 public class FirstServiceController {
 
     @GetMapping("/welcome")
     public String welcome(){
         return "Welcome to the First service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header){
+        log.info(header);
+        return "Hello First service";
     }
 }
